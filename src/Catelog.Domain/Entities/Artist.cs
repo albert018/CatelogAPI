07d5fdd635f47a20742c50753f40d77b9f964528
albert2.chen <albert2.chen@gmail.com>
2020-04-22 02:04:48 +0800

    0421

---
+++ src/Catelog.Domain/Entities/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catelog.Domain.Entities
+{
+    public class Artist
+    {
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
---
+++ src/Catelog.Domain/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catelog.Domain.Entities
+{
+    public class Genre
+    {
+        public Guid GenreId { get; set; }
+        public string GenreDescription { get; set; }
+    }
+}
